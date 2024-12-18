7f6ec92d8344ba7d87ce430d8a5de5243f2d4c4c
WinRAR <99762912+WINRARisyou@users.noreply.github.com>
2024-12-18 03:10:23 +0000

    make it actually work

--- workbox-config.js
+++ workbox-config.js
@@ -2,7 +2,7 @@ module.exports = {
 	maximumFileSizeToCacheInBytes: 5000000000,
 	globDirectory: 'celeste-wasm/',
 	globPatterns: [
-		'**/*.{ico,png,jpg,js,data,html,json,css,txt}'
+		'**/*.{ico,png,jpg,js,data,html,json,css,txt,wasm,dat}'
 	],
 	swDest: 'celeste-wasm/sw.js',
 	ignoreURLParametersMatching: [
